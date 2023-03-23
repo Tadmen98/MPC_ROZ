fae28594b1d3658365da484ef635801fb43e3718
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-03-23 11:35:07 +0100

    add function set_camera_parameters

--- src/base/pose_detector.py
+++ src/base/pose_detector.py
@@ -11,6 +11,15 @@ class PoseDetector(PoseDetectorBase):
         self.num_of_disparities = 64
         self.block_size = 15
 
+        self.focal_length = 700  # in pixels for HD720, for HD1080 use 1400
+        self.baseline = 0.063  # m
+
+        self.ppc_x = 0 #  x-coordinates of the principal point
+        self.ppc_y = 0 #  y-coordinates of the principal point
+
+        self.ppc_x_left = 0 #   x-coordinates of the principal points in the left image
+        self.ppc_x_right = 0 #   x-coordinates of the principal points in the right image
+
         self.model_point_cloud = None
         self.stero_bm = None
 
@@ -23,6 +32,14 @@ class PoseDetector(PoseDetectorBase):
 
     def init_model_point_cloud(self, point_cloud):
         self.model_point_cloud = point_cloud
+
+    def set_camera_parameters(self, focal_length, baseline, ppc_x, ppc_y, ppc_x_left, ppc_x_right):
+        self.focal_length = focal_length
+        self.baseline = baseline
+        self.ppc_x = ppc_x
+        self.ppc_y = ppc_y
+        self.ppc_x_left = ppc_x_left
+        self.ppc_x_right = ppc_x_right
 
     def create_stereo_corr_obj(self, num_of_disp=64, block_size=15):
         self.stero_bm = cv2.StereoBM_create(numDisparities=num_of_disp, blockSize=block_size)
