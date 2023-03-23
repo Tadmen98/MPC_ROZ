42937723ccb11b09b91bc65d49b06ea8fa6e2f76
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-03-23 09:00:30 +0100

    add empty camera slot and find objectin_3d_scene

--- src/base/3d_detector.py
+++ src/base/3d_detector.py
@@ -28,3 +28,32 @@ class PoseDetector:
     def gen_scene_point_cloud(self, img_left, img_right):
         disparity = self.stero_bm.compute(img_left, img_right)
 
+        focal_length = 0.8 * img_left.shape[1]  # assume a horizontal field of view of 60 degrees
+        baseline = 0.1  # assume a baseline of 10 cm
+        Q = np.float32([[1, 0, 0, -0.5 * img_left.shape[1]],
+                        [0, -1, 0, 0.5 * img_left.shape[0]],
+                        [0, 0, 0, -focal_length],
+                        [0, 0, 1 / baseline, 0]])
+        points_3d = cv2.reprojectImageTo3D(disparity, Q)
+
+        mask = disparity > disparity.min()
+        points_3d = points_3d[mask]
+
+        return points_3d
+
+    #@Slot()
+    def camera_slot(self, img_left, img_right):
+        object_coordinates = self.find_object_in_3d_scene(img_left, img_right)
+        #self.update_object_pose_signal.emit()
+
+    def find_object_in_3d_scene(self, img_left, img_right):
+        scene_point_cloud = self.gen_scene_point_cloud(img_left, img_right)
+
+        # Some logic
+
+        object_pose = None
+
+        return object_pose
+
+
+
